7b0854fde66e405dd04d48fc2e02b932cf682b8b
jordanthayer <jordan.thayer@gmail.com>
2016-05-10 11:02:52 -0400

    stubbing out minimax

---
+++ adversarial-search/src/minimax.ml
@@ -0,0 +1,18 @@
+(*
+  Simple implementation of the minimax algorithm for DLC Course
+  Jordan Thayer, May 2016
+*)
+
+(* Nodes come in two flavors, depending on who's turn it is to move.
+   Either it's a min move, or a max move.  The 'a just says we aren't
+   committing to the type of the remaining part of the node. *)
+type 'a node =
+| Min of 'a
+| Max of 'a
+
+let wrap fn = function
+  | Max s
+  | Min s -> fn s
+
+let minimax getChildren hasChildren getValue initState =
+  failwith "stub"
