dd819c05a876faed61315a385774a5bcc5f4fa56
jordanthayer <jordan.thayer@gmail.com>
2016-05-10 11:26:11 -0400

    simple minimax implementation for demonstration purposes

--- adversarial-search/src/minimax.ml
+++ adversarial-search/src/minimax.ml
@@ -6,15 +6,58 @@
 (* Nodes come in two flavors, depending on who's turn it is to move.
    Either it's a min move, or a max move.  The 'a just says we aren't
    committing to the type of the remaining part of the node. *)
+type player =
+| Min
+| Max
+
+let next = function
+  | Min -> Max
+  | Max -> Min
+
 type 'a node =
-| Min of 'a
-| Max of 'a
+  { data : 'a;
+    player : player;
+    score : int;
+    next : 'a node; }
 
-(* the game will provide functions on game states, it doesn't know about minimax's internal
-   representation! *)
-let wrap fn = function
-  | Max s
-  | Min s -> fn s
+let minimax getChildren getValue initState =
+  let makeNode cState player =
+    let rec ret = { data = cState;
+                    player = player;
+                    score = 0;
+                    next = ret; } in
+    ret in
+  let rec search node =
+    match getChildren node.data with
+      (* a terminal state, supply a score, and push it up *)
+    | [] ->   { node with score = (getValue node.data) }
+    | kids ->
+      begin
+        match List.fold_left (fun accum cState ->
+          let nextNode = makeNode cState node.player in
+          match accum with
+          | None -> Some nextNode
+          | Some accum ->
+            if node.player = Max && accum.score > nextNode.score
+            then Some accum
+            else if node.player = Max
+            then Some nextNode
+            else if node.player = Min && accum.score < nextNode.score
+            then Some accum
+            else Some nextNode) None kids with
+        | None -> failwith "?"
+        | Some best -> { node with score = best.score; next = best; }
+      end in
+  search (makeNode initState Max)
 
-let minimax getChildren hasChildren getValue initState =
-  failwith "stub"
+let main () =
+  let istate = Tic_tac_toe.initState in
+  let getValue s = Tic_tac_toe.toInt (Tic_tac_toe.getWinner s.Tic_tac_toe.board) in
+  let all_legal_moves (board : Tic_tac_toe.state) =
+    List.map (fun i -> { Tic_tac_toe.player = board.Tic_tac_toe.toPlay; Tic_tac_toe.ind = i; }) board.Tic_tac_toe.remaining in
+  let getChildren state =
+    let nexts = all_legal_moves state in
+    List.map (Tic_tac_toe.applyMove state) nexts in
+  minimax getChildren getValue istate
+
+let _ = main
