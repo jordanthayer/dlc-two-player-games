a146db59c3393479a6180972336df743cd1e51e3
jordanthayer <jordan.thayer@gmail.com>
2016-05-11 08:52:28 -0400

    added comments, since I'm lecturing from the code.

--- adversarial-search/src/minimax.ml
+++ adversarial-search/src/minimax.ml
@@ -1,62 +1,74 @@
-(*
+(**
   Simple implementation of the minimax algorithm for DLC Course
   Jordan Thayer, May 2016
 *)
 
-(* Nodes come in two flavors, depending on who's turn it is to move.
-   Either it's a min move, or a max move.  The 'a just says we aren't
-   committing to the type of the remaining part of the node. *)
+(** Nodes come in two flavors, depending on who's turn it is to move.
+    Either it's a min move, or a max move.  The 'a just says we aren't
+    committing to the type of the remaining part of the node. *)
 type player =
 | Min
 | Max
 
+(* Utility for figuring out what the next player is.
+   Min follows Max, Max follows min *)
 let next = function
   | Min -> Max
   | Max -> Min
 
-type 'a node =
-  { data : 'a;
-    player : player;
-    score : int;
-    next : 'a node; }
+type 'a node = {
+  data : 'a;         (* However the game wants to represent the game's state *)
+  player : player;   (* Who's turn is it to move? *)
+  score : int;       (* What's the Utility of this Move? *)
+  next : 'a node;    (* What's the next move to make in an optimal strategy? *)
+  }
 
-(* print out the solution minimax found for playing the game *)
+(** print out the solution minimax found for playing the game *)
 let rec visSolution toString node =
   Printf.printf "%s\n\n" (toString node.data);
-  if node.next != node then visSolution toString node.next
+  if node.next != node (* common trick for terminating trees without using Some / None *)
+  then visSolution toString node.next
 
-(* simple implementation of minimax. Note that we don't
-   need to know anything about the game specifically. Just how to get the next move. *)
-let minimax getChildren getValue initState =
+(** the types of the functions I'm relying on for minimax search **)
+type 'a succFun = 'a -> 'a list
+type 'a valueFun = 'a -> int
+
+(** simple implementation of minimax. Note that we don't
+    need to know anything about the game specifically. Just how to get the next move. *)
+let minimax (getChildren : 'a succFun) (getValue : 'a valueFun) (initState : 'a) =
+  (* a local helper function for adding nodes to the minimax tree *)
   let makeNode cState player =
+    (* recursive structure initially.  I am my own next node. *)
     let rec ret = { data = cState;
                     player = player;
                     score = 0;
                     next = ret; } in
     ret in
-  let rec search node =
+  let rec bestChild node accum cState =
+    let nextPlayer = next node.player in
+    let nextNode = search (makeNode cState nextPlayer) in
+    match accum with
+    | None -> Some nextNode
+    | Some accum ->
+      begin
+        match node.player with
+        | Max -> if accum.score >= nextNode.score then Some accum else Some nextNode
+        | Min -> if accum.score <= nextNode.score then Some accum else Some nextNode
+      end
+  and search node =
     match getChildren node.data with
       (* a terminal state, supply a score, and push it up *)
-    | [] ->   { node with score = (getValue node.data) }
-    | kids ->
+    | [] -> { node with score = (getValue node.data) }
+    | kids -> (* non-terminal state, search continues *)
       begin
-        match List.fold_left (fun accum cState ->
-          let nextNode = makeNode cState (next node.player) in
-          let nextNode = search nextNode in
-          match accum with
-          | None -> Some nextNode
-          | Some accum ->
-            begin
-              match node.player with
-              | Max -> if accum.score >= nextNode.score then Some accum else Some nextNode
-              | Min -> if accum.score <= nextNode.score then Some accum else Some nextNode
-            end) None kids with
+        match List.fold_left (bestChild node) None kids with
         | None -> failwith "?"
         | Some best -> { node with score = best.score; next = best; }
-      end in
+      end
+  in
   search (makeNode initState Max)
 
-(* example invocation assuming that we're going to play tic-tac-toe *)
+(** example invocation assuming that we're going to play tic-tac-toe *)
 let main () =
   let istate = Tic_tac_toe.initState in
   let getValue s = Tic_tac_toe.toInt (Tic_tac_toe.getWinner s.Tic_tac_toe.board) in
